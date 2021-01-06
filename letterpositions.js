2013098d4688fa55cd2dfd8393b601100f5932fe
Eric <ericos.bennett@gmail.com>
2021-01-06 22:56:53 +0000

    letter positions exercise

---
+++ letterpositions.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+      }
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+
+const assertArraysEqual = function(arr1, arr2) {
+  let msg = '✅✅✅ These arrays are identical!';
+  
+  if (arr1.length !== arr2.length) {
+    msg = '🛑🛑🛑 These arrays are not identical.';
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      msg = '🛑🛑🛑 These arrays are not identical.';
+    }
+  }
+  console.log(msg);
+};
+
+assertArraysEqual(letterPositions('hello')['h'], [0]);
+assertArraysEqual(letterPositions('hello')['e'], [1]);
+assertArraysEqual(letterPositions('hello')['l'], [2,3]);
+assertArraysEqual(letterPositions('hello')['o'], [4]);
